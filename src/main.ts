5d7c34d7ed9cdba8b66f7bb65c0c10c87c25c8e6
lapnv <lapnguyen12b@gmail.com>
2022-09-11 22:22:57 +0700

    config eslint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { env } from './config/env.config';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { env } from './config/env.config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT || 3000, () => {
-    console.log(env.DOMAN_BE);
+    console.log(env.DOMAN_BE)
   });
 }
 bootstrap();

407b61dd8ae50befab6d534195b7c51788811196
lnv1995 <lapnguyen12b@gmail.com>
2022-09-15 00:28:56 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { env } from 'process'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CatsModule } from './cats/cats.module'
